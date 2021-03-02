804ae588be055543f8a268909bbf68f39566c020
Jeffrey <ref6318@gmail.com>
2021-03-02 17:38:31 +0900

    docs: db 수정

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
+import path from 'path';
 
 class App {
     constructor(public express: any) {
@@ -7,6 +8,13 @@ class App {
 }
 
 const app = new App(express()).express;
+
+app.set('views', path.join(__dirname, '../views'));
+app.set('view engine', 'ejs');
+
+app.get("/", function(req: Request, res: Response) {
+    res.render('page/main');
+})
 
 app.listen(8080, function() {
     console.log("8080 server start");
--- src/model/db.ts
+++ src/model/db.ts
@@ -1,61 +1,117 @@
 import mysql from 'mysql';
 import dotenv from 'dotenv';
-import { DbQuery } from '../interfaces/db';
 dotenv.config();
 
-class GetConnect {
-    dbInfo: Object = {}
-    constructor() { }
+abstract class DbAbstract {
+    protected abstract sql: string;
+    protected abstract params: Array<String | Number>;
+    protected abstract callback: Function;
+}
 
-    dbConnection(): mysql.Connection {
-        this.dbInfo = {
+class Conn {
+    protected dbConnInfo: Object = {}
+    constructor() {
+        this.dbConnInfo = {
             host: process.env.DEV_DB_HOST,
             port: process.env.DEV_DB_PORT,
             user: process.env.DEV_DB_USER,
             password: process.env.DEV_DB_PASSWORD,
             database: process.env.DEV_DB_NAME,
-            dateStrings: [
+            dateStrings: [ 
                 'DATE',
                 'DATETIME'
             ],
-            multipleStatements: true
+            multipleStatements: true, //멀티 쿼리 활성화
         }
+    }
+}
 
-        const conn = mysql.createConnection(this.dbInfo);
+// class Pool extends Conn {
+//     constructor() {
+//         super();
+//     }
+//     dbConnection(): mysql.Pool {
+//         const poolInfo = {
+//             ...this.dbConnInfo,
+//             connectionLimit: 3
+//         }
+//         const pool = mysql.createPool(poolInfo);
+//         return pool;
+//     }
+// }
+
+//db 연결
+class GetConnect extends Conn {
+    constructor() {
+        super();
+    }
+
+    dbConnection(): mysql.Connection {
+        const conn = mysql.createConnection(this.dbConnInfo);
         conn.connect();
         return conn;
     }
 }
 
+//db 연결 끊기
 class CloseConnect {
     constructor(public conn: mysql.Connection) { }
-
     dbCloseConnection(): void {
         this.conn.end();
     }
 }
 
-export class Query implements DbQuery {
+//sql 트랜잭션
+export class Query extends DbAbstract {
     constructor(
-        public sql: string,
-        public params: Array<String | Number>,
-        public callback: Function
+        protected sql: string,
+        protected params: Array<String | Number>,
+        protected callback: Function
     ) {
-        this.dbQuery();
+        super(); //서브 클래스에서 상위 클래스를 호출할 때 사용하는 키워드, 부모가 갖고있는 기능과 자식인 자기도 갖고있는 부분을 super로 처리 
     }
 
-    private dbQuery(): void {
+    dbQuery(): void {
         const connSettings = new GetConnect();
         const conn = connSettings.dbConnection();
         const _this = this;
+        let result: Object = {}
 
-        conn.query(this.sql, this.params, function (err, results) {
+        conn.beginTransaction(function (err) {
             if (err) {
-                return console.error(err);
+                conn.rollback();
+                result = {
+                    status: 444, 
+                    message: "beginTransaction Error",
+                    error: err,
+                    data: {}
+                }
+                _this.callback(result);
+            } else {
+                conn.query(_this.sql, _this.params, function (err, results) {
+                    if (err) {
+                        conn.rollback();
+                        result = {
+                            status: 444,
+                            message: "query Error",
+                            error: err,
+                            data: {}
+                        }
+                    } else {
+                        conn.commit(function () {
+                            conn.end();
+                            result = {
+                                status: 222,
+                                message: "success",
+                                error: {},
+                                data: results
+                            }
+                            _this.callback(result);
+                        });
+                    }
+                });
             }
-            _this.callback(results);
         });
-
         this.dbClose(conn);
     }
 
@@ -64,3 +120,74 @@ export class Query implements DbQuery {
         connClose.dbCloseConnection();
     }
 }
+
+//pool 트랜잭션
+// export class PoolQuery extends DbAbstract {
+//     constructor(
+//         protected sql: string,
+//         protected params: Array<String | Number>,
+//         protected callback: Function
+//     ) {
+//         super();
+//     }
+
+//     dbQuery() {
+//         const poolSettings = new Pool();
+//         const pool = poolSettings.dbConnection();
+//         const _this = this;
+//         let result: Object = {};
+
+//         pool.getConnection(function (err, connection) {
+//             if (err) {
+//                 result = {
+//                     status: 444,
+//                     message: "pool getConnection Error",
+//                     error: err,
+//                     data: {}
+//                 }
+//                 return _this.callback(result);
+//             }
+//             connection.beginTransaction(function (err) {
+//                 if (err) {
+//                     connection.rollback(function () {
+//                         connection.release(); //반환
+//                         result = {
+//                             status: 444,
+//                             message: "Pool beginTransaction Error",
+//                             error: err,
+//                             data: {}
+//                         }
+//                         return _this.callback(result);
+//                     });
+//                 } else {
+//                     pool.query(_this.sql, _this.params, function (err, results) {
+//                         if (err) {
+//                             connection.rollback(function () {
+//                                 connection.release();
+//                             });
+//                             result = {
+//                                 status: 444,
+//                                 message: "Pool query Error",
+//                                 error: err,
+//                                 data: {}
+//                             }
+//                             return _this.callback(result);
+//                         } else {
+//                             connection.commit(function () {
+//                                 connection.end();
+//                                 connection.release();
+//                                 result = {
+//                                     status: 222,
+//                                     message: "Success",
+//                                     error: {},
+//                                     data: results
+//                                 }
+//                                return  _this.callback(result);
+//                             });
+//                         }
+//                     });
+//                 }
+//             });
+//         })
+//     }
+// }
