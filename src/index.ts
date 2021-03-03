9b150e5b5e8cbe1cc0c56decbbf440f16f57b23a
Jeffrey <ref6318@gmail.com>
2021-03-03 09:46:28 +0900

    docs: 폴더 추가

--- src/index.ts
+++ src/index.ts
@@ -2,15 +2,17 @@ import express, { Request, Response } from 'express';
 import path from 'path';
 
 class App {
-    constructor(public express: any) {
+    constructor(public express: express.Application) {
         this.express = express;
     }
 }
 
 const app = new App(express()).express;
 
+//ejs 적용
 app.set('views', path.join(__dirname, '../views'));
 app.set('view engine', 'ejs');
+//ejs 적용
 
 app.get("/", function(req: Request, res: Response) {
     res.render('page/main');
